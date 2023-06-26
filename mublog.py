820665cb161e2f6efa9fd072a40b2747e016a67b
Kuba Sunderland-Ober <kuba@mareimbrium.org>
2023-06-26 12:18:56 -0400

    Move configuration to a config file.
    Toml format would have been a good choice instead of ini, but
    it's only available in Python 3.11. To keep this working in older
    CPythons, ini is a safe bet.

--- mublog.py
+++ mublog.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+from configparser import ConfigParser, ExtendedInterpolation
 from glob import glob
 from string import Template
 import markdown2
@@ -7,19 +8,13 @@ import os
 import re
 import shutil
 
-dst_root_dir = "dst"
-dst_posts_dir = f"{dst_root_dir}/posts"
-dst_css_dir = f"{dst_root_dir}/css"
-dst_assets_dir = f"{dst_root_dir}/assets"
-src_root_dir = "src"
-src_posts_dir = f"{src_root_dir}/posts"
-src_css_dir = f"{src_root_dir}/css"
-src_assets_dir = f"{src_root_dir}/assets"
-post_ignore_delim = "_"
+config = ConfigParser(interpolation=ExtendedInterpolation())
+config.read("config.ini")
 
-author_name = "John Doe"
-author_mail = "johndoe@example.com"
-footer_copyright = f"&copy; 2023 {author_name}"
+
+def path(key: str, filename: str = None) -> str:
+    path = config['Paths'][key]
+    return f"{path}/{filename}" if filename else f"{path}/"
 
 
 def readfile(path: str) -> str:
@@ -43,22 +38,16 @@ def substitute(mapping: dict[str, str], in_path: str, out_path: str = None):
 
 
 def initialize_directories():
-    """Removes old build artefacts, and generates the build directories
-
-    The /dst directory is the root directory of the blog
-    The /dst/posts directory contains all the blog post files
-    The /dst/assets directory stores images, videos etc of posts
-    The /dst/css directory contains the style sheets of the blog
-    """
-    shutil.rmtree(dst_root_dir, ignore_errors=True)
+    """Removes old build artefacts, and generates the build directories"""
+    shutil.rmtree(path("dst_root"), ignore_errors=True)
 
     # Create output directories
-    for d in (dst_root_dir, dst_posts_dir, dst_css_dir, dst_assets_dir):
+    for d in (path("dst_root"), path("dst_posts"), path("dst_css"), path("dst_assets")):
         os.makedirs(d, exist_ok=True)
 
-    for f in glob(f"{src_css_dir}/*.css"):
-        shutil.copy(f, dst_css_dir)
-    shutil.copytree(src_assets_dir, dst_assets_dir, dirs_exist_ok=True)
+    for f in glob(path("src_css", "*.css")):
+        shutil.copy(f, path("dst_css"))
+    shutil.copytree(path("src_assets"), path("dst_assets"), dirs_exist_ok=True)
 
     print("Build directories initialized.")
 
@@ -81,34 +70,35 @@ def convert_md_file(src_md: str, dst_html: str, root: str) -> dict[str, str]:
     metadata = md.metadata
     metadata["src"] = src_md
     metadata["dst"] = dst_html
+    metadata["dst_link"] = dst_html.removeprefix(path("dst_root"))
 
     title = f"<title>{metadata['title']}</title>\n" if "title" in md.metadata else ""
 
     substitutions = {
-        "author_mail": author_mail,
+        "author_mail": config["Author"]["mail"],
         "contents": html,
-        "footer_copyright": footer_copyright,
+        "footer": config["Layout"]["footer"],
         "root": root,
         "title": title,
     }
-    substitute(substitutions, f"{src_root_dir}/post.html", dst_html)
+    substitute(substitutions, path("src_root", "post.html"), dst_html)
 
     return metadata
 
 
-def convert_md_files(path: str, root: str) -> list[dict[str, str]]:
+def convert_md_files(_path: str, root: str) -> list[dict[str, str]]:
     """
     Iterate through all .md files in a directory, write out the html converted result,
     and return a list of their metadata dictionaries.
     """
 
-    # Find all .md files in the directory, convert them, and collect metadata
+    draft_prefix = config["Paths"]["draft_prefix"]
     metadata_list = []
-    for src_post_path in glob(f"{path}/*.md"):
+    for src_post_path in glob(f"{_path}/*.md"):
         basename = os.path.basename(src_post_path)
-        if not basename.startswith(post_ignore_delim):
+        if not basename.startswith(draft_prefix):
             root_name, _ext = os.path.splitext(basename)
-            dst_post_path = f"{dst_posts_dir}/{root_name}.html"
+            dst_post_path = path("dst_posts", f"{root_name}.html")
 
             print(f'Processing post: {src_post_path}')
             metadata = convert_md_file(src_post_path, dst_post_path, root)
@@ -132,14 +122,14 @@ def sort_metadata(metadata: list[dict[str, str]]):
 
 if __name__ == "__main__":
     initialize_directories()
-    convert_md_file(f"{src_root_dir}/about.md",
-                    f"{dst_root_dir}/about.html", ".")
-    convert_md_file(f"{src_root_dir}/index.md",
-                    f"{dst_root_dir}/index.html", ".")
-    convert_md_file(f"{src_root_dir}/articles.md",
-                    f"{dst_root_dir}/articles.html", ".")
+    convert_md_file(path("src_root", "about.md"),
+                    path("dst_root", "about.html"), ".")
+    convert_md_file(path("src_root", "index.md"),
+                    path("dst_root", "index.html"), ".")
+    convert_md_file(path("src_root", "articles.md"),
+                    path("dst_root", "articles.html"), ".")
 
-    metadata_list = convert_md_files(src_posts_dir, "..")
+    metadata_list = convert_md_files(path("src_posts"), "..")
     sort_metadata(metadata_list)
 
     posts_processed = 0
@@ -153,7 +143,7 @@ if __name__ == "__main__":
         else:
             date = metadata["date"]
             title = metadata["title"]
-            dst_link = metadata["dst"][len("dst/"):]
+            dst_link = metadata["dst_link"]
             article_list += f'<li><b style="color: #14263b;">{date}</b> <a href="{dst_link}">{title}</a></li>\n'
             posts_processed += 1
 
@@ -161,6 +151,6 @@ if __name__ == "__main__":
 
     print("Generating article listing ...")
 
-    substitute({"articles": article_list}, f"{dst_root_dir}/articles.html")
+    substitute({"articles": article_list}, path("dst_root", "articles.html"))
 
     print(f"Finished! (built: {posts_processed}, skipped: {posts_skipped})")
